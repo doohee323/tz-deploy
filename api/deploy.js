5b27ff5bf3e276dda0f7a40c62104205a3cfbe06
Dewey Hong <dhong@fortinet.com>
2017-03-03 17:40:17 -0800

    .

--- api/deploy.js
+++ api/deploy.js
@@ -7,7 +7,7 @@
 /**
  */
 exports.deploy = function(req, res, cb) {
-	
+
 	var logger = require('../app.js').winston;
 	var config = require('../app.js').config;
 	var utils = require('../app.js').utils;
@@ -17,9 +17,9 @@ exports.deploy = function(req, res, cb) {
 	var async = require('async');
 	var sleep = require('sleep-promise');
 	var download = require('download-file');
-	
+
 	var next = cb;
-	if(!next) {
+	if (!next) {
 		var next = function(err, data) {
 			return utils.res(res, data);
 		}
@@ -30,29 +30,30 @@ exports.deploy = function(req, res, cb) {
 	logger.info(url);
 	request(url, function(err, response, body) {
 		logger.info(err)
+		logger.info("--------------ciJson:" + body)
 		var ciJson = JSON.parse(body);
-		var localJsonPath = config.deploy.sourceDir + 'mine.json';
-		logger.info(localJsonPath);
+		var mineJsonPath = config.deploy.sourceDir + 'mine.json';
+		logger.info(mineJsonPath);
 		async.waterfall([
 				function(callback) {
-					fs.readFile(localJsonPath, 'utf8', function(err, data) {
+					fs.readFile(mineJsonPath, 'utf8', function(err, data) {
 						if (err) {
 							logger.info(err)
 							throw err;
 						}
-						var localJson = JSON.parse(data);
+						var mineJson = JSON.parse(data);
 						// 2. comparing server's one with local one
-						if (ciJson.file != localJson.file || ciJson.version != localJson.version || ciJson.size == localJson.size) {
-							callback(null, localJson);
+						if (ciJson.file != mineJson.file || ciJson.version != mineJson.version || ciJson.size != mineJson.size) {
+							callback(null, mineJson);
 						} else { // 2
 							return next(0, []);
 						}
 					});
 				},
-				function(localJson, callback) {
-					logger.info("!!!!!localJsonPath: " + localJsonPath);
+				function(mineJson, callback) {
+					logger.info("!!!!!mineJsonPath: " + mineJsonPath);
 					// 3. gets new war, if different
-					url = config.deploy.ciServer + config.deploy.sourceDir + localJson.file;
+					url = config.deploy.ciServer + config.deploy.sourceDir + mineJson.file;
 					logger.info("downloading url: " + url + ' to ' + config.deploy.targetFile);
 					var options = {
 						directory : config.rootPath + '/' + config.deploy.sourceDir,
@@ -64,10 +65,10 @@ exports.deploy = function(req, res, cb) {
 							logger.info("Not found: " + url);
 							return next(0, []);
 						}
-						callback(null, localJson);
+						callback(null, mineJson);
 					})
 				},
-				function(localJson, callback) {
+				function(mineJson, callback) {
 					// 4. set local version and size with lastest one
 					var adresses = Object.keys(ifaces).reduce(function(result, dev) {
 						return result.concat(ifaces[dev].reduce(function(result, details) {
@@ -75,31 +76,31 @@ exports.deploy = function(req, res, cb) {
 						}, []));
 					});
 					logger.info(adresses)
-					localJson = ciJson;
-					localJson.ipaddress = adresses;
-					fs.writeFile(localJsonPath, JSON.stringify(localJson), 'utf8', function(err, data) {
+					mineJson = ciJson;
+					mineJson.ipaddress = adresses;
+					fs.writeFile(mineJsonPath, JSON.stringify(mineJson), 'utf8', function(err, data) {
 						if (err) {
 							logger.info(err)
 							callback(err, null);
 						}
-						callback(null, localJson);
+						callback(null, mineJson);
 					}); // 4
 				},
-				function(localJson, callback) {
+				function(mineJson, callback) {
 					// 5. set lock on repository
 					request.post(config.deploy.ciServer + 'lock', {
-						form : localJson
+						form : mineJson
 					}, function(err, response, body) {
 						if (err) {
 							logger.info(err)
 							callback(err, null);
 						} else {
 							logger.info(body)
-							callback(null, localJson);
+							callback(null, mineJson);
 						}
 					}); // 5
 				},
-				function(localJson, callback) {
+				function(mineJson, callback) {
 					// 6. deploy the lastest one
 
 					var cmd = 'sudo /bin/rm -rf ' + config.deploy.targetDir + '/' + config.deploy.targetFile;
@@ -110,22 +111,22 @@ exports.deploy = function(req, res, cb) {
 						if (err) {
 							logger.info("fail: 6. deploy the lastest one")
 						}
-						cmd = 'sudo /bin/mv ' + config.rootPath + '/' + config.deploy.sourceDir + localJson.file + ' '
+						cmd = 'sudo /bin/mv ' + config.rootPath + '/' + config.deploy.sourceDir + mineJson.file + ' '
 								+ config.deploy.targetDir + '/' + config.deploy.targetFile;
 						logger.info(cmd)
 						utils.runCommands([ cmd ], function(err, results) {
 							logger.info("==========err: " + err);
 							logger.info("==========results: " + results);
 							if (!err) {
-								callback(null, localJson);
+								callback(null, mineJson);
 							} else {
 								logger.info("fail: 6. deploy the lastest one")
-								// 7. set free on repository callback(null, localJson);
-								return setFree(localJson, next);
+								// 7. set free on repository callback(null, mineJson);
+								return setFree(mineJson, next);
 							}
 						}); // 6
 					}); // 6
-				}, function(localJson, callback) {
+				}, function(mineJson, callback) {
 					var num = Array.from(Array(config.deploy.checkCnt).keys());
 					config.req_done = false;
 					Object.keys(num).forEach(function(key, i) {
@@ -148,25 +149,25 @@ exports.deploy = function(req, res, cb) {
 									logger.info("---response.statusCode: " + response.statusCode);
 									if (response.statusCode == 200) {
 										config.req_done = true;
-										callback(null, localJson);
+										callback(null, mineJson);
 									}
 								}
 							});
 						}, i * 10000);
 					});
-				} ], function(err, localJson) {
-			// 7. set free on repository callback(null, localJson);
-			return setFree(localJson, next);
+				} ], function(err, mineJson) {
+			// 7. set free on repository callback(null, mineJson);
+			return setFree(mineJson, next);
 		})
 	})
 };
 
-var setFree = function(localJson, next) {
+var setFree = function(mineJson, next) {
 	var logger = require('../app.js').winston;
 	var request = require('request');
 	var config = require('../app.js').config;
 	request.post(config.deploy.ciServer + 'free', {
-		form : localJson
+		form : mineJson
 	}, function(err, response, body) {
 		if (!err && response.statusCode == 200) {
 			logger.info(body)
@@ -180,12 +181,12 @@ exports.lock = function(req, res, next) {
 	var config = require('../app.js').config;
 	var fs = require('fs');
 
-	var localJson = req.body;
+	var mineJson = req.body;
 	logger.info("req.body" + req.body)
 	var lockPath = config.rootPath + '/' + config.deploy.sourceDir + 'lock.json';
 	logger.info("--------------lockPath:" + lockPath)
-	logger.info("--------------localJson:" + JSON.stringify(localJson))
-	fs.writeFile(lockPath, JSON.stringify(localJson), 'utf8', function(err, data) {
+	logger.info("--------------mineJson:" + JSON.stringify(mineJson))
+	fs.writeFile(lockPath, JSON.stringify(mineJson), 'utf8', function(err, data) {
 		if (err) {
 			logger.info("write err" + err)
 			throw err;
