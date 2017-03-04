eabb0209c9758af76716cb2103fe5a1488036a03
Dewey Hong <dhong@fortinet.com>
2017-03-03 22:54:44 -0800

    .

---
+++ helpers/ready.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export PJT_DIR=/mnt/SodaTransferDeploy
+
+#path=`ls ${WORKSPACE}/target/*.war`
+path=`ls $1/target/*.war`
+echo $path
+cp $path $PJT_DIR/download
+chmod o+rw $PJT_DIR/download/*
+file=$(basename $path)
+
+pos=`expr index "$file" -`
+len=${#file}
+let len=len-pos
+version=${file:$pos:$len}
+version=${version//.war/}
+
+json='{"file": "'$file'", "version": "'$version'"}'
+echo $json
+echo $json > $PJT_DIR/download/lastest.json
+
+exit 0
