237e300afcadbf882b6675d30da98540ba64c556
Dewey Hong <dhong@fortinet.com>
2017-03-05 22:57:57 -0800

    .

--- helpers/ready_jar.sh
+++ helpers/ready_jar.sh
@@ -9,14 +9,18 @@ cp $path $PJT_DIR/download
 chmod o+rw $PJT_DIR/download/*
 file=$(basename $path)
 
+cd $PJT_DIR/download
 pos=`expr index "$file" -`
 len=${#file}
 let len=len-pos
 version=${file:$pos:$len}
 version=${version//.jar/}
+size=`du -b $file | cut -f1`
 
-json='{"file": "'$file'", "version": "'$version'"}'
+json='{"file": "'$file'", "version": "'$version'", "size": "'$size'"}'
 echo $json
 echo $json > $PJT_DIR/download/$1_lastest.json
+
+
 
 exit 0
